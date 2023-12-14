7581dccb1aa8892659935b44b815e808f7bb1644
devnetkid <jessefausett@gmail.com>
2023-12-13 21:38:19 -0700

    Added logging and variable constants to UPLINK and IP

--- src/meraki_scripts/celldata.py
+++ src/meraki_scripts/celldata.py
@@ -1,20 +1,27 @@
-"""Doc_String"""
+"""Creates a csv file showing loss, latency, and jitter"""
 
 import logging
+
 from meraki_scripts.universal import fileops, merakiops
 
 log = logging.getLogger(__name__)
-logging.basicConfig(filename='debug.log', level=logging.DEBUG)
+logging.basicConfig(filename="output/debug.log", level=logging.DEBUG)
+
+UPLINK = "cellular"
+IP = "8.8.8.8"
 
 def main():
     dashboard = merakiops.get_dashboard()
     orgs = merakiops.select_organization(dashboard)
-    networks = merakiops.select_network(dashboard, orgs[0])
-    sn = merakiops.get_mx_serial_number(dashboard, networks[0])
-    print(sn[1])
+    log.info(f"The selected organization is {orgs[1]}")
+    network = merakiops.select_network(dashboard, orgs[0])
+    log.info(f"The seleted network is {network[1]}")
+    pri_mx_sn = merakiops.get_mx_serial_number(dashboard, network[0])
+    log.info(f"The serial number found is {pri_mx_sn[1]}")
     try:
-        response = dashboard.devices.getDeviceLossAndLatencyHistory(sn[1], ip='8.8.8.8', uplink="wan1")
-        print(response)
+        response = dashboard.devices.getDeviceLossAndLatencyHistory(
+            pri_mx_sn[1], ip=IP, uplink=UPLINK
+        )
     except Exception as e:
         print(e)
     lines = []
@@ -24,7 +31,18 @@ def main():
         loss = stats["lossPercent"]
         latency = stats["latencyMs"]
         jitter = stats["jitter"]
-        line = start + "," + end + "," + str(loss) + "," + str(latency) + "," + str(jitter) + "\n"
+        line = (
+            start
+            + ","
+            + end
+            + ","
+            + str(loss)
+            + ","
+            + str(latency)
+            + ","
+            + str(jitter)
+            + "\n"
+        )
         lines.append(line)
     fileops.writelines_to_file("output/stats.csv", lines)
 
