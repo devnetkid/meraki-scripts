3a2b17c8ba8bc70d7b5d268cd0c1f9168ff5b9a4
devnetkid <jessefausett@gmail.com>
2024-04-22 11:35:25 -0600

    Added script to monitor API calls for an org

---
+++ src/meraki_scripts/apicalls.py
@@ -0,0 +1,32 @@
+"""Script to monitor API calls per second"""
+
+import logging
+
+from meraki_scripts.universal import fileops, merakiops
+
+log = logging.getLogger(__name__)
+fileops.setup_logging("apicalls")
+
+
+def main():
+    fileops.clear_screen()
+    log.info("Starting script apicalls")
+    settings = fileops.load_settings("input/settings.toml")
+    log.debug("The settings have been loaded from input/settings.toml")
+    print(fileops.colorme(settings["title"], "red"))
+    log.debug("Instantiating an instance of the Meraki dashboard")
+    dashboard = merakiops.get_dashboard()
+    org_id, org_name = merakiops.select_organization(dashboard)
+    orgname = fileops.colorme(org_name, "blue")
+    orgid = fileops.colorme(org_id, "blue")
+    log.info(f'The "{org_name}" organization with ID {org_id} has been selected')
+    print(f"The {orgname} organization with ID {orgid} has been selected\n")
+    output_file = settings["apicalls"]["output_file"]
+    response = dashboard.organizations.getOrganizationApiRequests(
+        org_id
+    )
+    fileops.writelines_to_file(output_file, response, "json")
+
+
+if __name__ == "__main__":
+    main()
